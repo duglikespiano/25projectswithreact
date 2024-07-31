6ff857f77dfdfe21df2f958236b32dea55e2b475
SUHO PARK <duglikespiano@gmail.com>
2024-08-01 00:58:30 +0900

    1 - ACCORDION REFACTORED

--- src/components/1_accordion/Accordion.tsx
+++ src/components/1_accordion/Accordion.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
 import Item from './Item';
 import { data } from './data';
 import styles from './Accordion.module.scss';
 
 export default function Accordion() {
-	const [items, setItems] = useState(data);
 	return (
 		<div className={styles['accordion']}>
 			<ul className={styles['items']}>
-				{items.map((item) => (
+				{data.map((item) => (
 					<Item id={item.id} title={item.title} description={item.description} />
 				))}
 			</ul>
